88201309aa981e6bb4f91477867ec39b0f2fe031
Ian Zamojc <izamojc@zamtools.com>
2022-01-16 04:42:30 -0500

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.lime,
+        secondary: colors.neutral,
+        success: colors.green,
+        warning: colors.amber,
+        danger: colors.red,
+        info: colors.sky,
+        disabled: colors.gray,
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+}

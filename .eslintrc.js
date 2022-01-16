d984f1ad91ebab43280c09a4c935ccc6288060b2
Ian Zamojc <izamojc@zamtools.com>
2022-01-16 04:09:08 -0500

    eslint and vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,21 @@ module.exports = {
   env: {
     // https://eslint.org/docs/rules/no-undef#nodejs
     node: true,
+    // ignore defineProps, defineEmits, etc.
+    'vue/setup-compiler-macros': true,
   },
-  parser: '@typescript-eslint/parser',
+  parser: 'vue-eslint-parser',
+  // https://github.com/vuejs/vue-eslint-parser#parseroptionsparser
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
   plugins: [
     '@typescript-eslint',
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
     'prettier',
   ],
 }

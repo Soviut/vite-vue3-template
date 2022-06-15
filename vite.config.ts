0df8fa91ae7ab2b16cfc39d0d03c8ea192f661ba
Jayvin <jayvin@arternal.com>
2022-06-15 17:59:16 -0400

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueI18n from '@intlify/vite-plugin-vue-i18n'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueI18n({
+      fullInstall: false, // do not install components and directives
+      include: path.resolve(__dirname, './src/locales/**'),
+    }),
+  ],
 
   resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
+    alias: [
+      { find: '@', replacement: '/src' },
+      {
+        find: 'vue-i18n',
+        replacement: 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js',
+      },
+    ],
   },
 })

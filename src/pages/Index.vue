98ed34c279e19f71875c2dc0b746ebf76d2af50a
Ian Zamojc <izamojc@zamtools.com>
2022-06-17 14:53:02 -0400

    fix page imports

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,3 +1,6 @@
+<!-- https://stackoverflow.com/questions/72639233/why-does-my-vue-file-cause-a-ts6504-error-and-ask-me-if-i-meant-to-allowjs -->
+<script setup lang="ts"></script>
+
 <template>
   <div>
     <header class="mb-8">

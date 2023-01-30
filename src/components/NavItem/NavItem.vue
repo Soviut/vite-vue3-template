c0c8b33f6552d9cf080adb0801470f861096bffb
Ian Zamojc <izamojc@zamtools.com>
2023-01-30 02:55:35 -0500

    started navitem

---
+++ src/components/NavItem/NavItem.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { FunctionalComponent } from 'vue'
+
+defineProps<{
+  to: string
+  icon?: FunctionalComponent
+}>()
+</script>
+
+<template>
+  <router-link
+    :to="to"
+    class="flex items-center p-5 hover:bg-white/5 hover:no-underline"
+  >
+    <component :is="icon" v-if="icon" />
+
+    <slot />
+  </router-link>
+</template>
